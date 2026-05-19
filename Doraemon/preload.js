fb99904705e46a55060f3ae2c4d7de9d2f07a23a
Roy Lee <lipeiheng4396@Gmail.com>
2026-05-19 16:25:34 +0800

    删除旧文件，准备上传新项目

--- Doraemon/preload.js
+++
@@ -1,8 +0,0 @@
-// 预加载脚本：在渲染进程加载前执行，用于安全地暴露主进程 API
-const { contextBridge, ipcRenderer } = require('electron');
-
-// 将 IPC 通信方法暴露给渲染进程（前端页面）
-contextBridge.exposeInMainWorld('electronAPI', {
-    // 设置鼠标穿透状态
-    setIgnoreMouseEvents: (ignore) => ipcRenderer.send('set-ignore-mouse-events', ignore)
-});
